81e9a901f3c5f413f1df7d8236acaef91275e46e
RÔMULO RODRIGUES DE OLIVEIRA <68561301+Romuloro@users.noreply.github.com>
2021-07-18 16:10:20 -0300

    commit_12_07_2021

---
+++ aulas_secao_17_propriedades.py
@@ -0,0 +1,33 @@
+"""
+Propriedades -
+
+"""
+
+
+class Conta:
+
+    contador = 0
+
+    def __init__(self, titular, saldo, limite):
+        self.__numero = Conta.contador + 1
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        return f'Saldo de {self.__saldo} do cliente {self.__titular}'
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.__saldo -= valor
+        destino.__saldo += valor
+
+
+
+
+
